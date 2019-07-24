a05fb34752a87f79afe5eef5bd1cdc83b0de42e5
25801 <yuan.wang07@hand-china.com>
2019-07-24 19:19:51 +0800

    123

---
+++ precache-manifest.1713bc5fa1d17abc86317b518d96d975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf36d364623c63925c471e8108b1cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f1b4416a29bc38fa74",
+    "url": "/static/css/main.7d198186.chunk.css"
+  },
+  {
+    "revision": "342b763149df18a11f74",
+    "url": "/static/js/2.374d8ee3.chunk.js"
+  },
+  {
+    "revision": "b6f1b4416a29bc38fa74",
+    "url": "/static/js/main.d9a63978.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
